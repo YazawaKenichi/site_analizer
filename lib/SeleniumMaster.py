6c6fbd5df51fc136336fb30cc3bd41bc7ceed8fc
YazawaKenichi <tonfarken83825@gmail.com>
2024-03-23 04:33:20 +0900

    使用感の変更

--- lib/SeleniumMaster.py
+++ lib/SeleniumMaster.py
@@ -8,10 +8,12 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from PrintMaster import Printer
+from optparse import OptionParser
 
 class Browser:
     def __init__(self, browser = "/usr/bin/chromium-browser", driver = "/usr/bin/chromiumdriver", ui = False):
         self.ui = ui
+        browser = self.getArgs()
         self.initSelenium(browser, ui)
         if self.ui:
             self.printer = Printer()
@@ -20,6 +22,24 @@ class Browser:
                     "screen-full" : True
                     }
             self.printer.setConfig(self.config)
+
+    def getArgs(self):
+        parser = OptionParser()
+        parser.add_option(
+                "--driver",
+                default = "/usr/bin/chromiumdriver",
+                type = "string",
+                dest = "driver",
+                help = "chromiumdriver の場所"
+                )
+        parser.add_option(
+                "--browser",
+                default = "/usr/bin/chromium-browser",
+                type = "string",
+                dest = "browser",
+                help = "chromium-browser の場所"
+                )
+        return parser.parse_args()
 
     # ブラウザを動かすためのクラスを作成する
     def initSelenium(self, browser = "/usr/bin/chromium-browser", driver = "/usr/bin/chromiumdriver"):
@@ -40,11 +60,11 @@ class Browser:
         time.sleep(delay)
 
     # URL で指定したサイトの HTML を全て読み込ませてから取得する
-    def getSoup(self):
+    def getSoup(self, cookies = None):
         # HTML ソースを取得
         html = self.driver.page_source
         # bs4 型に作成
-        soup = BeautifulSoup(html, "lxml")
+        soup = BeautifulSoup(html, "lxml", cookies = cookies)
         return soup
 
     def reload(self, num = "", ui = True):
